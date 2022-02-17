e3afb56b777b5b1fe88828e3f2f7446a35f8d74d
annalazarenkova <73938092+annalazarenkova@users.noreply.github.com>
2022-02-17 08:51:23 +0200

    simple frogger

---
+++ style.js
@@ -0,0 +1,49 @@
+body {
+  margin-left: 50px;
+  margin-top: 50px;
+}
+button {
+  margin-bottom: 20px;
+}
+
+.grid {
+  border: solid black;
+  height: 180px;
+  width: 180px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.grid div {
+  height: 20px;
+  width: 20px;
+}
+
+.ending-block {
+  background-color: red;
+}
+
+.starting-block {
+  background-color: blue;
+}
+
+.l1,
+.l2,
+.l3 {
+  background-color: brown;
+}
+.l4,
+.l5 {
+  background-color: lightskyblue;
+}
+
+.c1{
+  background-color: black;
+}
+.c2, .c3 {
+  background-color: lightslategray;
+}
+
+.frog {
+  background-color: green;
+}
